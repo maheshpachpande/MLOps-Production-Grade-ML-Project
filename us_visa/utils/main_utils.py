f431f91fdf17bfa5279b155d52fac045cc49e2ae
maheshpachpande <pachpandemahesh300@gmail.com>
2025-05-29 23:10:32 +0530

    utils added

---
+++ us_visa/utils/main_utils.py
@@ -0,0 +1,119 @@
+import os
+import sys
+
+import numpy as np
+import dill
+import yaml
+from pandas import DataFrame
+
+from us_visa.exception import USvisaException
+from us_visa.logger import logging
+
+# Utility functions for US Visa application processing
+# read_yaml_file: Reads a YAML file and returns its content as a dictionary.
+def read_yaml_file(file_path: str) -> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+
+    except Exception as e:
+        raise USvisaException(e, sys) from e
+    
+
+# write_yaml_file: Writes a dictionary to a YAML file, optionally replacing the file if it exists.
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise USvisaException(e, sys) from e
+    
+
+
+# load_object: Loads a Python object from a file using dill serialization.
+# file_path: str - The path to the file containing the serialized object.
+def load_object(file_path: str) -> object:
+    logging.info("Entered the load_object method of utils")
+
+    try:
+
+        with open(file_path, "rb") as file_obj:
+            obj = dill.load(file_obj)
+
+        logging.info("Exited the load_object method of utils")
+
+        return obj
+
+    except Exception as e:
+        raise USvisaException(e, sys) from e
+    
+
+# save_numpy_array_data: Saves a numpy array to a file in binary format.
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise USvisaException(e, sys) from e
+    
+
+
+# load_numpy_array_data: Loads a numpy array from a file in binary format.
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise USvisaException(e, sys) from e
+
+
+
+# save_object: Saves a Python object to a file using dill serialization.
+def save_object(file_path: str, obj: object) -> None:
+    logging.info("Entered the save_object method of utils")
+
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+        logging.info("Exited the save_object method of utils")
+
+    except Exception as e:
+        raise USvisaException(e, sys) from e
+
+
+# drop_columns: Drops specified columns from a pandas DataFrame.
+def drop_columns(df: DataFrame, cols: list)-> DataFrame:
+
+    """
+    drop the columns form a pandas DataFrame
+    df: pandas DataFrame
+    cols: list of columns to be dropped
+    """
+    logging.info("Entered drop_columns methon of utils")
+
+    try:
+        df = df.drop(columns=cols, axis=1)
+
+        logging.info("Exited the drop_columns method of utils")
+        
+        return df
+    except Exception as e:
+        raise USvisaException(e, sys) from e
